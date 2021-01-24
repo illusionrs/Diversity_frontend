34a82816e1da224989ddb2c39c30e3f89e62610f
Rajeev Shukla <rajeevshukla255@gmail.com>
2021-01-24 22:41:11 +0530

    intialized

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React ,{useState} from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App(){
+
+
+
+    const [formUsername, setUsername] = useState("")
+    const [formPassword, setPassword] = useState("")
+
+
+    function formHandle(e){
+        
+        e.preventDefault()
+
+        setUsername("")
+        setPassword("")
+    }
+
+
+    return <> 
+         
+     <form onSubmit={formHandle}>
+         <input type="text" placeholder="username" value={formUsername} onChange={(e)=> setUsername(e.target.value)}/>
+         <input type="password" placeholder="Password" value={formPassword} onChange={(e)=> setPassword(e.target.value)}/>
+         <input type="submit" value="Log In"/>
+     </form>
+    
+    </>
+
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"))
